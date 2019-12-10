ed1a0930df8088149a4a32fa0889f128ad659a98
A. Moskaljuk <alekseiko@gmail.com>
2019-12-10 17:01:05 +0100

    implemented the classes to pass the test

---
+++ src/main/java/book_exercise/SecurityService.java
@@ -0,0 +1,5 @@
+package book_exercise;
+
+public interface SecurityService {
+    String md5(String password);
+}
---
+++ src/main/java/book_exercise/User.java
@@ -0,0 +1,7 @@
+package book_exercise;
+
+public interface User {
+    String getPassword();
+
+    void setPassword(String passwordMd5);
+}
