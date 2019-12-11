0cc37abb0ffe5046b2675940f024db8be8f08fb2
A. Moskaljuk <alekseiko@gmail.com>
2019-12-11 20:26:39 +0100

    race result service test pass

--- src/main/java/book_exercise_5_7_2/RaceResultsService.java
+++ src/main/java/book_exercise_5_7_2/RaceResultsService.java
@@ -1,9 +1,14 @@
 package book_exercise_5_7_2;
 
 public class RaceResultsService {
+
+    private Client client;
+
     public void addSubscriber(Client client) {
+        this.client = client;
     }
 
     public void send(Message message) {
+        client.receive(message);
     }
 }
