4f8e95653ae6fe4ec1edb11a4bfdfc5f65109e4c
A. Moskaljuk <alekseiko@gmail.com>
2019-12-11 21:12:45 +0100

    refactored tests

--- src/test/java/book_exercise_5_7_2/RaceResultsServiceTest.java
+++ src/test/java/book_exercise_5_7_2/RaceResultsServiceTest.java
@@ -2,34 +2,48 @@ package book_exercise_5_7_2;
 
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class RaceResultsServiceTest {
 
-    @Test
-    public void subscribedClientShouldReceiveMessage() {
-        RaceResultsService raceResults = new RaceResultsService();
-        Client client = mock(Client.class);
-        Message message = mock(Message.class);
+    private RaceResultsService raceResults = new RaceResultsService();
+    private Message message = mock(Message.class);
+    private Client clientA = mock(Client.class, "clientA");
+    private Client clientB = mock(Client.class, "clientB");
 
-        raceResults.addSubscriber(client);
+    @Test
+    public void notSubscribedClientShouldNotReceiveMessage() {
         raceResults.send(message);
-        verify(client).receive(message);
+
+        verify(clientA, never()).receive(message);
+        verify(clientB, never()).receive(message);
     }
 
     @Test
-    public void messageShouldBeSentToAllSubscribedClients() {
-        RaceResultsService raceResults = new RaceResultsService();
-        Client clientA = mock(Client.class, "clientA");
-        Client clientB = mock(Client.class, "clientB");
-        Message message = mock(Message.class);
+    public void subscribedClientShouldReceiveMessage() {
+        raceResults.addSubscriber(clientA);
+        raceResults.send(message);
 
+        verify(clientA).receive(message);
+    }
+
+    @Test
+    public void allSubscribedClientsShouldReceiveMessages() {
         raceResults.addSubscriber(clientA);
         raceResults.addSubscriber(clientB);
+
         raceResults.send(message);
 
         verify(clientA).receive(message);
         verify(clientB).receive(message);
     }
+
+    @Test
+    public void shouldSendOnlyOneMessageToMultiSubscriber() {
+        raceResults.addSubscriber(clientA);
+        raceResults.addSubscriber(clientA);
+        raceResults.send(message);
+        verify(clientA).receive(message);
+    }
+
 }
